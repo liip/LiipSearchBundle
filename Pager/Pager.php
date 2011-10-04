bf1175d15d5ef67e4ad77516234952a88811b7fd
Dan <dan@dc-home.ch>
2011-10-04 20:31:14 +0200

    Added license

--- Pager/Pager.php
+++ Pager/Pager.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Liip/SearchBundle
+ *
+ * (c) Liip AG
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Liip\SearchBundle\Pager;
 
 class Pager
